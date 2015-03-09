85d1c66667bb60662bb48c811f0df3ec05b4e880
pitjdm <piotr.krupa91@gmail.com>
2015-03-10 00:02:48 +0100

    print, add_front 90%

--- Source.c
+++ Source.c
@@ -16,9 +16,31 @@ void l_init(type_dlistVar *List){
 	List->count = 0;
 }
 
+void l_print(type_dlistVar *List){
+	type_dlist *p;
+	p = List->head;
+	while(p){
+		printf("asd %d: %d", List->count, p->data);
+		p = p->next;
+	}
+}
+
+void l_add_front(type_dlistVar *List){
+	type_dlist *p;
+	p = malloc(sizeof(type_dlist));
+	printf("type int:");
+	scanf_s("%d", p->data);
+	p->prev = NULL;
+	p->next = List->head;
+	List->tail = p;
+	List->count++;
+
+}
+
 int main(){
 
 	type_dlistVar *List;
+
 	printf("asd");
 	getchar();
 	return EXIT_SUCCESS;
